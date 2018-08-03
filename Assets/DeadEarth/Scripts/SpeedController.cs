39fa5ef6d4fa06bc7ca095b86948a6e338593413
Darkpony <dponyg@gmail.com>
2018-08-02 21:48:33 -0700

    Pushing Lesson 14

---
+++ Assets/DeadEarth/Scripts/SpeedController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour {
+
+    //private
+    private Animator _controller = null;
+
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+        _controller = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _controller.SetFloat("speed", speed);
+	}
+}
---
+++ Assets/KeepSceneFocused.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class KeepSceneFocused : MonoBehaviour {
+
+    public bool FocusScene;
+
+	// Use this for initialization
+	void Start () {
+
+        if(FocusScene && Application.isEditor)
+        {
+            EditorWindow.FocusWindowIfItsOpen(typeof (SceneView));
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
